d51aaba7a742c1746405eedac9e3e2d4737bf75a
Rashi Tripathi <rashi.s.tripathi@gmail.com>
2026-03-26 20:11:46 +0530

    relocated DFS code and added explanation of the multi-path o/p

--- Examples/multi_path_demo.py
+++ Examples/multi_path_demo.py
@@ -1,7 +1,14 @@
 #Multi-path STA using a simple engine
 
-from sta_engine import compute_Tcomb, check_setup, check_hold, build_graph, find_all_paths
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from sta_engine import compute_Tcomb, check_setup, check_hold, build_graph
 from circuit_models import example_multi_path_circuit
+from graph_utils import find_all_paths
+
 
 
 # Main STA Flow
@@ -53,3 +60,51 @@ if __name__ == "__main__":
     print("Critical Path:")
     print(" -> ".join(critical_path))
     print(f"Worst Setup Slack = {worst_setup_slack:.2f} ns")
+
+
+# ===================== STA CALCULATION (THIS CIRCUIT) =====================
+
+# Paths found:
+# 1) FF1 → G1 → G2 → FF2
+# 2) FF1 → G1 → FF3 → G2 → FF2
+
+# --------------------- Tcomb ---------------------
+# Path 1:
+#   = 1.5 + 2.0 + 3.0 = 6.5 ns
+#
+# Path 2:
+#   = 1.5 + 1.0 + 0.8 + 3.0 = 6.3 ns
+
+# --------------------- SETUP SLACK ---------------------
+# Formula:
+#   Setup Slack = Tclk - (Tcq + Tcomb + Tsetup)
+
+# Path 1:
+#   = 10 - (1 + 6.5 + 2.5)
+#   = 10 - 10
+#   = 0 ns  → borderline PASS
+
+# Path 2:
+#   = 10 - (1 + 6.3 + 2.5)
+#   = 10 - 9.8
+#   = 0.2 ns  → PASS
+
+# --------------------- HOLD SLACK ----------------------
+# Formula:
+#   Hold Slack = (Tcq_min + Tcomb) - Thold
+
+# Path 1:
+#   = (0.5 + 6.5) - 1
+#   = 7.0 - 1
+#   = 6.0 ns  → PASS
+
+# Path 2:
+#   = (0.5 + 6.3) - 1
+#   = 6.8 - 1
+#   = 5.8 ns  → PASS
+
+# --------------------- CRITICAL PATH ---------------------
+# Path with minimum setup slack:
+#   FF1 → G1 → G2 → FF2 (Setup Slack = 0 ns)
+
+# ========================================================================
--- sta_engine.py
+++ sta_engine.py
@@ -36,28 +36,4 @@ def build_graph(edges):
     return graph, delays
 
 
-# using DFS to find all paths
-def find_all_paths(graph, start, end, path=None):
-    if path is None: 
-        path = [] # when used for the first time, intialize path as empty list
 
-    path = path + [start] # we add path so far + current node
-
-    if start == end:
-        return [path] 
-    """ 
-    as DFS runs, start keeps changing, so like initially start=FF1, next G1, next G2, next FF2
-    find_all_paths(graph, "G2", "FF2", ["FF1","G1"]), then
-    find_all_paths(graph, "FF2", "FF2", ["FF1","G1","G2"])
-       now start == end == "FF2", so we return [path] which is [["FF1","G1","G2","FF2"]] """
-
-    if start not in graph:
-        return []
-    #If start is not the destination, and start has no outgoing edges, it means no path exists, so return empty list
-
-    paths = []
-    for node in graph[start]:
-        if node not in path:
-            paths.extend(find_all_paths(graph, node, end,    path))
-
-    return paths
